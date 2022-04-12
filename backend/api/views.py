484cc00d1505ee37fb82727a502dc83b7449ee4b
Aditya-Mallick <aditymallick90aa@gmail.com>
2022-04-12 17:05:58 +0530

    Posting data with drf

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,19 @@
 import json
 from django.http import JsonResponse
-from django.forms.models import model_to_dict
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from yaml import serialize
 
 from product.models import Product
 from product.serializers import ProductSerializers
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
-    # DRF API View
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        data = ProductSerializers(instance).data
-    return Response(data)
+    serializer = ProductSerializers(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "Not good data"})
--- py_client/basics.py
+++ py_client/basics.py
@@ -2,7 +2,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-response = requests.get(endpoint, params={'abc': 1}, json={
-                        "query": "This is parameter json"})
+response = requests.post(
+    endpoint, json={"title": "This is hello ", "price": "Hello from basic"})
 print(response.json())
 print(response.status_code)
